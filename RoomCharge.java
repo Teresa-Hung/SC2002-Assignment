35409988fef5cb2acf6baad4acdca37bf9157a3e
IDunKn0w <101077421+IDunKn0w@users.noreply.github.com>
2022-04-03 23:23:21 +0800

    Add files via upload

---
+++ RoomCharge.java
@@ -0,0 +1,26 @@
+package miniproj;
+
+public class RoomCharge { 	
+	private int numOfDays, i;
+	private double roomCharge;
+	
+	BasicRoomCharge brc;
+	DayTypeCharge dtc;
+	
+	public void setNumOfDays(int days) {
+		numOfDays = days;
+	}
+	
+	public int getNumOfDays() {
+		return numOfDays;
+	}
+	
+	public double roomRate() {
+		roomCharge = 0.0;
+		for(i=0;i<numOfDays;i++) {
+			roomCharge = roomCharge + brc.baseCharge()*dtc.typeCharge();
+		}
+		
+		return roomCharge;
+	}
+}
