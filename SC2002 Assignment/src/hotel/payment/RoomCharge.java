bf22aa545319074c24b74600d4c7459342adf202
IDunKn0w <101077421+IDunKn0w@users.noreply.github.com>
2022-04-13 05:48:28 +0800

    Update RoomCharge.java

--- SC2002 Assignment/src/hotel/payment/RoomCharge.java
+++ SC2002 Assignment/src/hotel/payment/RoomCharge.java
@@ -1,24 +1,33 @@
 package payment;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
 public class RoomCharge { 	
-	private int numOfDays, i;
+	private long numOfDays, i;
 	private double roomCharge;
+	private LocalDate dateIn, dateOut;
+	private DayOfWeek day;
 	
 	BasicRoomCharge brc;
 	DayTypeCharge dtc;
 	
-	public void setNumOfDays(int days) {
-		numOfDays = days;
-	}
-	
-	public int getNumOfDays() {
-		return numOfDays;
-	}
-	
 	public double roomRate() {
+		dateIn = getCheckInDate();
+		dateOut = getCheckOutDate(); 
+		
+		numOfDays = ChronoUnit.DAYS.between(dateIn, dateOut);
+		day = dateIn.getDayOfWeek();
+		
 		roomCharge = 0.0;
 		for(i=0;i<numOfDays;i++) {
-			roomCharge = roomCharge + brc.getBaseCharge()*dtc.typeCharge();
+			roomCharge = roomCharge + brc.getBaseCharge()*dtc.typeCharge(day);
+			day = day.plus(1);
 		}
 		
 		return roomCharge;
