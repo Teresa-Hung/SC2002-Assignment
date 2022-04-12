2ce7374dec3721da523fc83d1b6aed3d905f0c1e
IDunKn0w <101077421+IDunKn0w@users.noreply.github.com>
2022-04-13 05:50:36 +0800

    Update DayTypeCharge.java

--- SC2002 Assignment/src/hotel/payment/DayTypeCharge.java
+++ SC2002 Assignment/src/hotel/payment/DayTypeCharge.java
@@ -1,12 +1,14 @@
 package payment;
-import reservation.Reservation
+import java.time.DayOfWeek;
 
 public class DayTypeCharge {
 	private double dayTypeCharge;
+	private DayOfWeek day;
 	
 	public double getTypeCharge(DayOfWeek dayType) {
+		day = dayType;
 		
-		switch(dayType) {
+		switch(day) {
 		case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY:
 			dayTypeCharge = 1.0;
 		case SATURDAY, SUNDAY:
