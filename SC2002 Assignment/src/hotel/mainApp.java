dd4ef0b061dc06b66ef022c161f8451166086163
Brybn <101080960+Brybn@users.noreply.github.com>
2022-04-14 15:19:07 +0800

    Update mainApp.java

--- SC2002 Assignment/src/hotel/mainApp.java
+++ SC2002 Assignment/src/hotel/mainApp.java
@@ -2,11 +2,11 @@ package sc2002Proj;
 import java.util.Scanner;
 
 public class mainApp {
-
+	RoomManager rm = new RoomManager("48_Hotel_Rooms.txt");
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hotel reservation system!");
-		int choice1;
+		String choice1;
 		do {
 			System.out.println("1) Guest Information.");
 			System.out.println("2) Reservation Information.");
@@ -14,29 +14,30 @@ public class mainApp {
 			System.out.println("4) Room Service Information.");
 			System.out.println("5) Payment Information.");
 			System.out.println("6) Quit");
-			choice1 = sc.nextInt();
+			choice1 = sc.next();
 			switch(choice1) {
-				case 1:
+				case "1":
 					GuestManager manager = new GuestManager();
 					System.out.println("1) Update Guest Details.");
 					System.out.println("2) Search Guest.");
-					int a = sc.nextInt();
+					String a = sc.next();
 					switch(a) {
-						case 1:
+						case "1":
 							Guest g = manager.updateDetails();
 							if(g != null)
 								manager.displayGuestDetails(g);
 							break;
-						case 2:
+						case "2":
 							Guest guest1 = manager.searchGuest();
 							if(guest1 != null) {
 								System.out.println("Guest found.");
 								manager.displayGuestDetails(guest1);
 							}
 							break;
+						default:
+							System.out.println("Invalid choice.");
 					}
-					
-				case 2:
+				case "2":
 					//reservation functions
 					System.out.println("1) Create Reservation.");
 					System.out.println("2) Update Reservation.");
@@ -48,41 +49,35 @@ public class mainApp {
 					
 					
 					}
-
-					
-				case 3:
-					//room functions
-					
-				case 4:
+				case "3":
+					rm.roomUI();
+					break;
+				case "4":
 					//room service functions
 					System.out.println("1) Create order");
 					System.out.println("2) Get room's orders");
 					System.out.println("3) Get the menu");
 					System.out.println("4) Get list of orders");
 					System.out.println("5) Update order status");
-					int choice = sc.nextInt();
+					String choice = sc.next();
 					switch(choice) {
-						case 1:{
+						case "1":
 							System.out.println("Enter room number");
 							String room = sc.nextLine();
 							OrderManager.saveOrder("order.txt",room);
 							break;
-						}
-						case 2:{
+						case "2":
 							System.out.println("Enter room number");
 							String room = sc.nextLine();
 							OrderManager.getOrder("order.txt",room, true);
 							break;
-						}
-						case 3:{
+						case "3":
 							Menu.printMenu("order.txt");
 							break;
-						}
-						case 4:{
+						case "4":
 							OrderManager.printOrder("order.txt");
 							break;
-						}
-						case 5:{
+						case "5":
 							System.out.println("Enter room number");
 							String room = sc.nextLine();
 							System.out.println("Enter order id");
@@ -93,15 +88,19 @@ public class mainApp {
 							sc.nextLine();
 							OrderManager.changeStatus("order.txt", orderID, room, OrderStatus.values()[status]);
 							break;
-						}
-					
-				case 5:
-					
-					
-
+						default:
+							System.out.println("Invalid choice.");
+					}
+					break;
+				case "5":
+					// payment
+					break;
+				default: 
+					System.out.println("Invalid choice.");
 			}
-		}while(choice1 != 6);
-
+		}while(!choice1.equals("6"));
+			
+	rm.writeRoomList("48_Hotel_Rooms.txt") // end of program save room list
 	}
 
 }
