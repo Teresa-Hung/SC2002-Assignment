d67057d193876ffd767d34ed89ff426a1ed79297
bachts <64970131+bachts@users.noreply.github.com>
2022-04-14 11:46:53 +0800

    Update Order.java

--- SC2002 Assignment/src/hotel/roomservice/Order.java
+++ SC2002 Assignment/src/hotel/roomservice/Order.java
@@ -1,24 +1,47 @@
 import java.time.LocalTime;
+import java.util.Scanner;
+import java.io.IOException;
+enum OrderStatus{RECEIVED, PREPARING, READY}
 public class Order {
-	public Order(int id, MenuItems item,LocalTime time, String remarks, String status) {
+	public Order(int id, int itemID, int number, MenuItems item, LocalTime time, String note, OrderStatus temp) {
 		this.orderID = id;
+		this.itemID = itemID;
+		this.roomNumber = number;
 		this.item = item;
 		this.orderTime = time;
-		this.remarks = remarks;
-		this.orderStatus = status;
+		this.remarks = note;
+		this.status = temp;
 	}
-	public void updateOrder(Order e, String newStatus) {
-		e.orderStatus = newStatus;
+	public int getID() {
+		return this.orderID;
 	}
-	public String getStatus(Order e) {
-		return e.orderStatus;
+	public int getRoom() {
+		return this.roomNumber;
 	}
-	private MenuItems item;
+	public LocalTime getTime() {
+		return this.orderTime;
+	}
+	public void updateOrder(OrderStatus newStatus) {
+		this.status = newStatus;
+	}
+	public String getRemarks() {
+		return this.remarks;
+	}
+	public OrderStatus getStatus() {
+		return this.status;
+	}
+	public MenuItems getItem() {
+		return this.item;
+	}
 	private int orderID;
+	private int itemID;
+	private int roomNumber;
+	private MenuItems item;
 	private LocalTime orderTime;
 	private String remarks;
-	private String orderStatus;
+	private OrderStatus status;
 }
 // create order
 // make a list of order
 // add price to invoice
+// 
