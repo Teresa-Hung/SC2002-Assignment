284e4f2c16246e65bf03b1bcc92e4bdacdd0325d
Brybn <101080960+Brybn@users.noreply.github.com>
2022-04-15 00:10:40 +0800

    Update mainApp.java

--- SC2002 Assignment/src/hotel/mainApp.java
+++ SC2002 Assignment/src/hotel/mainApp.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 import guest.Guest;
 import guest.GuestManager;
-import room.Room.RoomStatus;
-import room.RoomManager;
+import room.*;
+import roomservice.Order.OrderStatus;
 import roomservice.OrderManager;
 
 public class mainApp {
@@ -57,19 +57,20 @@ public class mainApp {
 					}
 					break;
 				case "3":
-					rm.roomUI();
+					roomUI(rm, sc);
 					break;
 				case "4":
-					//room service functions
+					// room service functions
 					System.out.println("1) Create order");
 					System.out.println("2) Delete order");
 					System.out.println("3) Get the menu");
 					System.out.println("4) Get list of orders");
 					System.out.println("5) Update order status");
+					String room;
 					switch(sc.nextLine()) {
 						case "1":
 							System.out.println("Enter room number");
-							String room = sc.nextLine();
+							room = sc.nextLine();
 							if(rm.getRoomStatus(room) == RoomStatus.VACANT) {
 								System.out.println("This room is empty");
 								break;
@@ -78,7 +79,7 @@ public class mainApp {
 							break;
 						case "2":
 							System.out.println("Enter room number");
-							String room = sc.nextLine();
+							room = sc.nextLine();
 							if(rm.getRoomStatus(room) == RoomStatus.VACANT) {
 								System.out.println("This room is empty");
 								break;
@@ -94,7 +95,7 @@ public class mainApp {
 							break;
 						case "5":
 							System.out.println("Enter room number");
-							String room = sc.nextLine();
+							room = sc.nextLine();
 							if(rm.getRoomStatus(room) == RoomStatus.VACANT) {
 								System.out.println("This room is empty");
 								break;
@@ -109,6 +110,7 @@ public class mainApp {
 						default:
 							System.out.println("Invalid choice.");
 					}
+					break;
 				case "5":
 					Payment pay = new Payment();
 					Promotion promo = new Promotion();
@@ -192,10 +194,157 @@ public class mainApp {
 							System.out.println("Invalid choice.");
 					}
 					break;
-				default: 
+				case "6":
+					rm.writeRoomList("48_Hotel_Rooms.txt"); //save room list before program exit
+					break;
+				default:
 					System.out.println("Invalid choice.");
 			}
 		} while(!choice1.equals("6"));
-	rm.writeRoomList("48_Hotel_Rooms.txt"); // end of program save room list
+	}
+	
+	private static void roomUI(RoomManager rm, Scanner sc) {
+		String choice;
+		do {
+			System.out.println("-------------------------------\n"
+					 + "Room Menu:\n"
+			        	 + "(1) Print Room Occupancy Report\n"
+			        	 + "(2) Print Room Status Report\n"
+			        	 + "(3) Print Room Report\n"
+			        	 + "(4) Print Room Details\n"
+			        	 + "(5) Update Room Details\n"
+			        	 + "(6) Exit\n"
+			        	 + "-------------------------------");
+			choice = sc.nextLine();
+			switch (choice) {
+			case "1":
+				rm.printOccupancyReport();
+				break;
+			case "2":
+				rm.printStatusReport();
+				break;
+			case "3":
+				rm.printRoomReport();
+				break;
+			case "4":
+				System.out.print("Enter room number: ");
+				rm.printRoomDetails(sc.nextLine());
+				break;
+			case "5":
+				updateRoomDetailsUI(rm, sc);
+				break;
+			case "6":
+				rm.writeRoomList("48_Hotel_Rooms.txt");
+				break;
+			default:
+				System.out.println("Invalid option.");
+			}
+		} while (!choice.equals("6"));
+	}
+
+	private static void updateRoomDetailsUI(RoomManager rm, Scanner sc) {
+		System.out.print("Enter room number: ");
+		String roomNumber = sc.nextLine();
+		if (rm.findRoom(roomNumber) == null) return;
+		System.out.print("--------------------\n"
+				+ "Update Details Menu:\n"
+				+ "(1) Room Number\n"
+				+ "(2) Room Type\n"
+				+ "(3) Bed Type\n"
+				+ "(4) Room Status\n"
+				+ "(5) Toggle WiFi\n"
+				+ "(6) Toggle Smoking\n"
+				+ "(7) Toggle Balcony\n"
+				+ "(8) Max Size\n"
+				+ "--------------------\n"
+				+ "Enter option: ");
+		switch (sc.nextLine()) {
+		case "1":
+			System.out.print("Enter new room number: ");
+			String newRoomNumber = sc.nextLine();
+			if (newRoomNumber.length() != 4) {
+				System.out.println("Invalid room number format.");
+				return;
+			}
+			try {
+				// if new room number is not numeric, parseInt throws NumberFormatException
+				Integer.parseInt(newRoomNumber);
+				boolean valid = roomNumber.charAt(0) == newRoomNumber.charAt(0) && roomNumber.charAt(1) == newRoomNumber.charAt(1);
+				if (!valid) {
+					System.out.println("Invalid room number format.");
+					return;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid room number format.");
+				return;
+			}
+			if (rm.findRoom(newRoomNumber, false) != null) {
+				System.out.println("New room number already exists.");
+				return;
+			}
+			rm.updateRoomNumber(roomNumber, newRoomNumber);
+			System.out.println("Room number set to " + newRoomNumber);
+			break;
+		case "2":
+			System.out.print("Options: SINGLE, DOUBLE, SUITE, VIP_SUITE\n"
+					+ "Enter new room type: ");
+			try {
+				RoomType newRoomType = RoomType.valueOf(sc.nextLine().toUpperCase());
+				rm.updateRoomType(roomNumber, newRoomType);
+				System.out.println("Room type set to " + newRoomType);
+			} catch (Exception e) {
+				System.out.println("Invalid room type.");
+			}
+			break;
+		case "3":
+			System.out.print("Options: TWIN, QUEEN, KING\n"
+					+ "Enter new bed type: ");
+			try {
+				BedType newBedType = BedType.valueOf(sc.nextLine().toUpperCase());
+				rm.updateBedType(roomNumber, newBedType);
+				System.out.println("Bed type set to " + newBedType);
+			} catch (Exception e) {
+				System.out.println("Invalid bed type.");
+			}
+			break;
+		case "4":
+			System.out.print("Options: VACANT, OCCUPIED, RESERVED, UNDER_MAINTENANCE\n"
+					+ "Enter new room status: ");
+			try {
+				RoomStatus newRoomStatus = RoomStatus.valueOf(sc.nextLine().toUpperCase());
+				rm.updateRoomStatus(roomNumber, newRoomStatus);
+				System.out.println("Room status set to " + newRoomStatus);
+			} catch (Exception e) {
+				System.out.println("Invalid room status.");
+			}
+			break;
+		case "5":
+			boolean wifiEnabled = rm.isRoomWifiEnabled(roomNumber);
+			rm.updateRoomWifi(roomNumber, !wifiEnabled);
+			System.out.println("WiFi Enabled set to " + !wifiEnabled + ".");
+			break;
+		case "6":
+			boolean smoking = rm.isRoomSmoking(roomNumber);
+			rm.updateRoomSmoking(roomNumber, !smoking);
+			System.out.println("Smoking set to " + !smoking + ".");
+			break;
+		case "7":
+			boolean balcony = rm.roomHasBalcony(roomNumber);
+			rm.updateRoomBalcony(roomNumber, !balcony);
+			System.out.println("Balcony set to " + !balcony + ".");
+			break;
+		case "8":
+			System.out.print("Enter new max size: ");
+			try {
+				int i = Integer.parseInt(sc.nextLine());
+				rm.updateRoomMaxSize(roomNumber, i);
+				System.out.println("Max size set to " + i);
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid size.");
+			}
+			break;
+		default:
+			System.out.println("Invalid option.");
+		}
 	}
 }
